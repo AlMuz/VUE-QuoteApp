29b50af354cb9503f4fa28e0b00ed1ae90123d17
AlMuz <almuz@inbox.lv>
2019-07-07 11:10:58 +0300

    creating app components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="container">
-    
+
     </div>
 </template>
 
 <script>
     export default {
-        
+      data: function() {
+        return {
+          quoutes: [
+            'Just a quoute to see smth'
+          ],
+          maxQuoutes: 10
+        }
+      }
     }
 </script>
 
